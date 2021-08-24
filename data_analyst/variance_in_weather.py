43d3aaac3aed7197b5f8c41c86ac9c9bd7052434
mark <mebowersock@gmail.com>
2021-08-24 08:54:09 -0500

    Create variance_in_weather.py

---
+++ data_analyst/variance_in_weather.py
@@ -0,0 +1,23 @@
+import codecademylib3_seaborn
+import pandas as pd
+import numpy as np
+from weather_data import london_data
+
+print(london_data.head())
+print(len(london_data))
+
+temp=london_data.TemperatureC
+average_temp=np.average(temp)
+temperature_var=np.var(temp)
+temperature_standard_deviation=np.std(temp)
+
+june=london_data.loc[london_data['month']==6]['TemperatureC']
+july=london_data.loc[london_data['month']==7]['TemperatureC']
+
+print(np.mean(june),np.mean(july))
+print(np.std(june),np.std(july))
+
+for i in range(1, 13):
+  month = london_data.loc[london_data["month"] == i]["TemperatureC"]
+  print("The mean temperature in month "+str(i) +" is "+ str(np.mean(month)))
+  print("The standard deviation of temperature in month "+str(i) +" is "+ str(np.std(month)) +"\n")
